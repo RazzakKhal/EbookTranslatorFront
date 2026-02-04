281192bbfb86c6bbc8128869b4e1d83c6ac6b59b
razzak <khalfallah.razzak@gmail.com>
2026-02-04 21:42:42 +0100

    ebt-7: Composant d’import de fichier EPUB

--- src/app/features/translate/pages/translator-main-page/translator-main-page.ts
+++ src/app/features/translate/pages/translator-main-page/translator-main-page.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { PInputComponent } from '../../../../design-system/atoms/pinput-component/pinput-component';
+import { SlicePipe } from '@angular/common';
 
 @Component({
   selector: 'app-translator-main-page',
-  imports: [],
+  imports: [PInputComponent, SlicePipe],
   templateUrl: './translator-main-page.html',
   styleUrl: './translator-main-page.css',
 })
 export class TranslatorMainPage {
+  readonly file = signal<File | null>(null);
 
+  onFileSelected(file: File | null) {
+    this.file.set(file);
+  }
 }
