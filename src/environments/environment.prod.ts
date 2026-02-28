c01e047418948ef18e994ef2a4f631a79c72d423
razzak <khalfallah.razzak@gmail.com>
2026-02-28 17:24:13 +0100

    ebt-38: Mettre en place authorization grant flow PKCE

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api.monsite.com',
+  keycloakUrl: 'https://auth.monsite.com',
+  clientId: 'ebook-angular',
+  realm: 'ebookTranslator',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000',
+  keycloakUrl: `http://localhost:8081`,
+  clientId: `ebook-angular`,
+  realm: `ebookTranslator`,
 };
